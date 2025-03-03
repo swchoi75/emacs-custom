5ef650b3f6980542645d10a58f0572fa44bb225c
Sungwook Choi <swchoi75@gmail.com>
2025-03-03 19:14:54 +0900

    python

--- init.el
+++ init.el
@@ -1,5 +1,17 @@
-;; (load "~/.config/emacs-custom/lisp/init-python.el")
-
-(load "~/.config/emacs-custom/lisp/init-R.el")
 (load "~/.config/emacs-custom/lisp/init-meow.el")
+(load "~/.config/emacs-custom/lisp/init-python.el")
+(load "~/.config/emacs-custom/lisp/init-R.el")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(highlight-indent-guides company-anaconda anaconda-mode pyvenv python-black meow ess company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,4 +1,44 @@
-(use-package elpy
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+
+(use-package python-black
   :ensure t
-  :init
-  (elpy-enable))
+  :bind (("C-c b" . python-black-buffer)))
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode 1))
+
+(use-package anaconda-mode
+  :ensure t
+  :bind (("C-c C-x" . next-error))
+  :config
+  (require 'pyvenv)
+  (add-hook 'python-mode-hook 'anaconda-mode))
+
+(use-package company-anaconda
+  :ensure t
+  :config
+  (eval-after-load "company"
+   '(add-to-list 'company-backends '(company-anaconda :with company-capf))))
+
+(use-package highlight-indent-guides
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'highlight-indent-guides-mode)
+  (setq highlight-indent-guides-method 'character))
+
+
+(setq python-shell-interpreter "~/.virtualenvs/py312/bin/ipython")
+
+(set-language-environment "UTF-8")
+(setq use-short-answers t)
+
+(require 'recentf)
+(recentf-mode 1) 
